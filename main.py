5d89c290ae4c9a6b962401e5a39d5d75bc0a1623
Kostanios <kotov.const@mail.ru>
2022-07-27 20:43:54 +0300

    feat: create model

--- main.py
+++ main.py
@@ -1,1 +1,180 @@
-from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, SpatialDropout1D, BatchNormalization, Embedding, Flatten, Conv1D, MaxPooling1D
+from tensorflow.keras.utils import plot_model
+from sklearn.metrics import confusion_matrix
+
+from prepareData import VOCAB_SIZE, WIN_SIZE, x_train, y_train, x_test, y_test, x_val, y_val
+
+CLASS_LIST = ['neutral', 'pump', 'dump']
+
+def make_mod(
+    VOCAB_SIZE,
+    WIN_SIZE,
+    CLASS_COUNT
+):
+
+    model = Sequential()
+    model.add(Embedding(VOCAB_SIZE, 10, input_length=WIN_SIZE))
+    model.add(SpatialDropout1D(0.2))
+    model.add(BatchNormalization())
+    model.add(Conv1D(20, 3, activation='relu'))
+    model.add(Conv1D(20, 3, activation='relu'))
+    model.add(MaxPooling1D(2))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(CLASS_COUNT, activation='softmax'))
+    return model
+
+def compile_train_model(
+    model,
+    x_train,
+    y_train,
+    x_val,
+    y_val,
+    optimizer='adam',
+    epochs=50,
+    batch_size=128,
+    figsize=(20, 5)
+):
+    model.compile(optimizer=optimizer,
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    plot_model(model, dpi=60, show_shapes=True)
+
+    history = model.fit(x_train,
+                        y_train,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_data=(x_val, y_val))
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=figsize)
+    fig.suptitle('model learning plot')
+    ax1.plot(history.history['accuracy'],
+               label='train accuracy')
+    ax1.plot(history.history['val_accuracy'],
+               label='test accuracy')
+    ax1.xaxis.get_major_locator().set_params(integer=True)
+    ax1.set_xlabel('epoch')
+    ax1.set_ylabel('share of correct answers')
+    ax1.legend()
+
+    ax2.plot(history.history['loss'],
+               label='train mistakes')
+    ax2.plot(history.history['val_loss'],
+               label='validate mistakes')
+    ax2.xaxis.get_major_locator().set_params(integer=True)
+    ax2.set_xlabel('Epoch')
+    ax2.set_ylabel('Mistakes')
+    ax2.legend()
+    plt.show()
+
+    return model
+
+def eval_model(
+    model,
+    x,
+    y_true,  # text sample of text marks
+    class_labels,
+    cm_round=2,  # round parameter
+    title='',
+    figsize=(25, 25)
+):
+
+    y_pred = model.predict(x)
+
+    # mistakes matrix
+    cm = confusion_matrix(np.argmax(y_true, axis=1),
+                          np.argmax(y_pred, axis=1),
+                          normalize='true')
+
+    # matrix round
+    cm = np.around(cm, cm_round)
+
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.set_title(f'Neural network {title}: normalized mistakes matrix', fontsize=18)
+    plt.gca().images[-1].colorbar.remove()
+    plt.xlabel('prediction classes', fontsize=26)
+    plt.ylabel('actual classes', fontsize=26)
+    fig.autofmt_xdate(rotation=45)
+    plt.show()
+
+    print('-' * 100)
+    print(f'neuralnetwork: {title}')
+
+    for cls in range(len(class_labels)):
+        # max confidence
+        cls_pred = np.argmax(cm[cls])
+        msg = 'true :-)' if cls_pred == cls else 'false :-('
+        print('Class: {:<20} {:3.0f}% classify as {:<20} - {}'.format(
+            class_labels[cls],
+            100. * cm[cls, cls_pred],
+            class_labels[cls_pred],
+            msg
+        ))
+
+    print('\naverage accuracy: {:3.0f}%'.format(100. * cm.diagonal().mean()))
+
+def compile_train_eval_model(
+    model,
+    x_train,
+    y_train,
+    x_test,
+    y_test,
+    class_labels=CLASS_LIST,
+    title='',
+    optimizer='adam',
+    epochs=50,
+    batch_size=128,
+    graph_size=(20, 5),
+    cm_size=(15, 15)
+):
+
+    model = compile_train_model(model,
+                                x_train, y_train,
+                                x_test, y_test,
+                                optimizer=optimizer,
+                                epochs=epochs,
+                                batch_size=batch_size,
+                                figsize=graph_size)
+
+
+    eval_model(model, x_test, y_test,
+               class_labels=class_labels,
+               title=title,
+               figsize=cm_size)
+
+
+    return model
+
+model_Conv_1 = make_mod(VOCAB_SIZE, WIN_SIZE, 3)
+
+mymodel = compile_train_eval_model(
+    model_Conv_1,
+    x_train, y_train,
+    x_test, y_test,
+    optimizer='adam',
+    epochs=50,
+    batch_size=200,
+    class_labels=CLASS_LIST,
+    title='elonmask tweets'
+)
+
+y_pred = mymodel.predict(x_val)
+
+print(y_pred)
+print(y_val)
+
+# Разберем результаты предсказания
+
+r = np.argmax(y_pred, axis=1)
+unique, counts = np.unique(r, return_counts=True)
+counts = counts/y_pred.shape[0]*100
+print(unique, counts)
+
--- prepareData.py
+++ prepareData.py
@@ -1,8 +1,12 @@
 import tweepy  # https://github.com/tweepy/tweepy
-from secret import consumer_secret, consumer_key
 import datetime
 import pandas as pd
+import numpy as np
 import os
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras import utils
+
+from secret import consumer_secret, consumer_key
 
 # Twitter API credentials
 
@@ -10,6 +14,13 @@ def parce_string_to_date (string_date):
     year, month, day = string_date.split('-')
     return datetime.date(int(year), int(month), int(day))
 
+
+def write_tweets_to_file(tweets):
+    print(len(tweets))
+    for tweet in tweets:
+        with open(f'tweets/{tweet[0]}.txt', 'w', encoding="utf-8") as f:
+            f.write(f'{tweet[0]}////{tweet[1]}////{tweet[2]}')
+    pass
 
 def download_tweets(
     screen_name
@@ -64,8 +75,9 @@ candlesList = []
 
 x_data = []
 y_data = []
+# download tweets and write to file
+# write_tweets_to_file(download_tweets("elonmusk"))
 
-# allTweets = download_tweets("elonmusk")
 allTweets = []
 all_tweet_files = os.listdir("tweets/")
 for file_path in all_tweet_files:
@@ -95,10 +107,10 @@ for rowIndex in range(len(df['Date'])):
         different_type = 0
         ratio = startPrice / nextPrice
         if ratio > 1.001:
-            different_type = 1
-        if ratio < 0.999:
             different_type = 2
-
+        if ratio < 0.999:
+            different_type = 1
+        # print(different_type)
         # For future calculate
         #
         # different = nextPrice - startPrice
@@ -107,26 +119,125 @@ for rowIndex in range(len(df['Date'])):
 
         # datePriceDict[df['Date'][rowIndex]] = df['Adj Close'][rowIndex]
 
-        print(startCandleDate)
-        print(startPrice)
-        print(endCandleDate)
-        print(nextPrice)
+        # print(startCandleDate)
+        # print(startPrice)
+        # print(endCandleDate)
+        # print(nextPrice)
         startCandleDate = endCandleDate
         startPrice = nextPrice
-        print(ratio)
-        print(tweetIndex)
+        # print(ratio)
+        # print(tweetIndex)
         while allTweets[tweetIndex][1] < endCandleDate:
             x_data.append(allTweets[tweetIndex][2])
             y_data.append(different_type)
             tweetIndex = tweetIndex + 1
-        print(tweetIndex)
-        print('--------')
+        # print(tweetIndex)
+        # print('--------')
 
 # print(datePriceDict)
 
-def write_tweets_to_file(tweets):
-    print(len(tweets))
-    for tweet in tweets:
-        with open(f'tweets/{tweet[0]}.txt', 'w', encoding="utf-8") as f:
-            f.write(f'{tweet[0]}////{tweet[1]}////{tweet[2]}')
-    pass
+def make_tokenizer(
+    VOCAB_SIZE,
+    txt_train
+):
+    tokenizer = Tokenizer(num_words=VOCAB_SIZE,
+                          filters='!"#$%&()*+,-–—./…:;<=>?@[\\]^_`{|}~«»\t\n\xa0\ufeff',
+                          lower=True,
+                          split=' ',
+                          oov_token='unrecognized-word',
+                          char_level=False)  # prohibit tokenize every symbol
+
+    tokenizer.fit_on_texts(txt_train)
+    return tokenizer
+
+def make_train_test(
+        tokenizer,
+        txt_train,
+        txt_test=None
+):
+    # transform word to tokens
+    seq_train = tokenizer.texts_to_sequences(txt_train)
+
+    if txt_test:
+        # transform word to tokens
+        seq_test = tokenizer.texts_to_sequences(txt_test)
+    else:
+        seq_test = None
+
+    return seq_train, seq_test
+
+def split_sequence(
+        sequence,
+        win_size,
+        hop
+):
+    return [sequence[i:i + win_size] for i in range(0, len(sequence) - win_size + 1, hop)]
+
+
+def vectorize_sequence(
+        seq_list,
+        y_list,
+        win_size,
+        hop
+):
+    tweets_count = len(seq_list)
+    x, y = [], []
+
+    for tweet_index in range(tweets_count):
+        vectors = split_sequence(seq_list[tweet_index], win_size, hop)
+        x += vectors
+        y += [utils.to_categorical(y_list[tweet_index], 3)] * len(vectors)
+
+    return np.array(x), np.array(y)
+
+
+def make_train_test(
+        tokenizer,
+        txt_train,
+        txt_test=None,
+        txt_val=None
+):
+    # transform word to tokens
+    seq_train = tokenizer.texts_to_sequences(txt_train)
+
+    if txt_test:
+        # transform word to tokens
+        seq_test = tokenizer.texts_to_sequences(txt_test)
+    else:
+        seq_test = None
+    if txt_val:
+        # transform word to tokens
+        seq_val = tokenizer.texts_to_sequences(txt_test)
+    else:
+        seq_val = None
+
+    return seq_train, seq_test, seq_val
+
+
+VOCAB_SIZE = 20000
+WIN_SIZE = 8
+WIN_HOP = 1
+train_text_size = 3000
+test_text_size = 100
+val_text_size = 100
+
+text_train = x_data[:train_text_size]
+classes_train = y_data[:train_text_size]
+text_test = x_data[train_text_size:train_text_size+test_text_size]
+classes_test = y_data[train_text_size:train_text_size+test_text_size]
+text_val = x_data[train_text_size+test_text_size:]
+classes_val = y_data[train_text_size+test_text_size:]
+
+tok = make_tokenizer(VOCAB_SIZE, text_train)
+seq_train, seq_test, seq_val = make_train_test(tok, text_train, text_test, text_val)
+
+print("Фрагмент обучающего текста:")
+print("В виде оригинального текста:              ", text_train[0][:101])
+print("Он же в виде последовательности индексов: ", seq_train[0][:20])
+
+x_train, y_train = vectorize_sequence(seq_train, classes_train, WIN_SIZE, WIN_HOP)
+x_test, y_test = vectorize_sequence(seq_test, classes_test, WIN_SIZE, WIN_HOP)
+x_val, y_val = vectorize_sequence(seq_val, classes_val, WIN_SIZE, WIN_HOP)
+
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
